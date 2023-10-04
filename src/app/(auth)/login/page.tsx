a064fa863e5819ae96aa4df58a33ce6c4bc87721
Lucas Ribeiro <lucasribsa@icloud.com>
2023-10-04 12:52:48 -0300

    adicionando tela sign-in

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,53 @@
 'use client';
-import { Link } from '@chakra-ui/next-js';
+import * as S from './styled';
+import { EmailIcon, LockIcon } from '@chakra-ui/icons';
+import {
+  FormLabel,
+  Input,
+  Image,
+  InputLeftElement,
+  Stack,
+  InputGroup
+} from '@chakra-ui/react';
 
 const Login = () => {
   return (
     <main>
-      <h1>ORGANIZA LOGIN</h1>
-      <Link href={'/'} color="blue.400" _hover={{ color: 'blue.500' }}>
-        IR PARA HOME
-      </Link>
+      <S.Form>
+        <Image
+          boxSize="350px"
+          src="images/logo_transparent.png"
+          alt="Foto da logo"
+        />
+        <FormLabel width={390}>E-mail</FormLabel>
+        <Stack spacing={4} />
+        <InputGroup width={400} mb={10}>
+          <InputLeftElement pointerEvents="none">
+            <EmailIcon color="gray.50" />
+          </InputLeftElement>
+          <Input
+            type="email"
+            placeholder="example@email.com"
+            bg="gray.800"
+            color={'white'}
+          />
+        </InputGroup>
+        <FormLabel width={390}>Sua senha</FormLabel>
+        <Stack spacing={4} />
+        <InputGroup width={400} mb={20}>
+          <InputLeftElement pointerEvents="none">
+            <LockIcon color="gray.50" />
+          </InputLeftElement>
+          <Input
+            type="password"
+            placeholder="*********"
+            bg="gray.800"
+            color={'white'}
+          />
+        </InputGroup>
+        <S.Botao>Entrar</S.Botao>
+        <S.Botao>Cadastrar-se</S.Botao>
+      </S.Form>
     </main>
   );
 };
---
+++ src/app/(auth)/login/styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+export const Form = styled.form `
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+
+`;
+
+export const Botao = styled.button `
+    background-color: #1A202C;
+    color: #fff;
+    margin-bottom: 2.5%;
+    width: 25em;
+    height: 3em;
+
+`;
